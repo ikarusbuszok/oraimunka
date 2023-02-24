5c6eeee910395965dec5fe6e7dae983327269dbb
ikarusbuszok <115149206+ikarusbuszok@users.noreply.github.com>
2023-02-24 12:40:17 +0100

    metod

---
+++ OneDrive/Documents/NetBeansProjects/metodus/src/metodus/Metodus.java
@@ -0,0 +1,35 @@
+package gyakorlok;
+
+import java.util.Random;
+
+
+public class Gyakorlok {
+
+    
+    public static void main(String[] args) {
+      equalizer_5_sorbol();
+      
+    }
+
+    private static void equalizer_5_sorbol() {
+       eq();
+       eq();
+       eq();
+       eq();
+       eq();
+       eq();
+       
+    }
+
+    private static void eq() {
+        int db = rnd.nextInt(3, 8);
+        String szin = "uu001b[45m";
+        for (int = 0; i < db; i++) {
+            System.out.print(szin + " ");
+    }
+      System.out.println(" ");
+        
+        
+    }
+    
+}
